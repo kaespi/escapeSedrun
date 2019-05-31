74e5c2c1249626a77ca5c339bd20f0fc5ea428a6
kaespi <kg@kgmw.ch>
2019-05-31 18:23:03 +0200

    controlPanel: add basic structure to activate the time machine based on analog input

--- timeMachine/controlPanel/controlPanel.ino
+++ timeMachine/controlPanel/controlPanel.ino
@@ -15,20 +15,53 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#define WIFI_ANALOG_PIN 0   //!< analog pin for reading the status of the "WiFi buttons"
+#define DEBUG                       //!< enable debug output over the serial line
 
-void setup() {
+#define WIFI_ANALOG_PIN 0           //!< analog pin for reading the status of the "WiFi buttons"
+#define ANALOG_3P3V_PIN_THRES 338   //!< threshold for deciding whether an input pin is 1 or 0 (3.3V pin) (=round(3.3/5*1023/2))
+
+//! type for the state of the time machine
+typedef enum TIME_MACHINE_STATE_e
+{
+    TIME_MACHINE_OFF = 0,   //!< time machine off
+    TIME_MACHINE_ON  = 1    //!< time machine on/enabled
+} TIME_MACHINE_STATE_t;
+
+//! current state of the time machine (initialized to off)
+static TIME_MACHINE_STATE_t stTimemachine;
+
+//! Initialization
+void setup()
+{
     // for debugging purposes, initialize a serial connection
     Serial.begin(115200);
     delay(10);
+
+    // ************** TIME MACHINE INITIALIZATION **************
+    stTimemachine = TIME_MACHINE_OFF;
 }
 
-void loop() {
-    // read the value from the sensor:
-    int pinVal = analogRead(WIFI_ANALOG_PIN);
-    Serial.print("pin value");
-    Serial.println(pinVal);
+//! Continuous running loop
+void loop()
+{
+    if (stTimemachine==TIME_MACHINE_OFF)
+    {
+        // read the value from the sensor to see if the time machine
+        // is activated
+        int pinVal = analogRead(WIFI_ANALOG_PIN);
+        if (pinVal > ANALOG_3P3V_PIN_THRES)
+        {
+            stTimemachine = TIME_MACHINE_ON;
 
-    // wait for half a second
-    delay(500);
+#ifdef DEBUG
+            Serial.print("Time machine activated (");
+            Serial.print(pinVal);
+            Serial.println(")");
+#endif
+        }
+    }
+    else if (stTimemachine==TIME_MACHINE_ON)
+    {
+        // time machine activated and running
+    }
 }
